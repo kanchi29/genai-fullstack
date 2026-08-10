4fb4be12c7cf0490115a7ad7fc836b8f917fd99e
kanchi29 <kanchisri29@gmail.com>
2026-08-10 13:44:16 +0530

    Operator Overloading

---
+++ Python/01_Datatypes/06_operator_overloading.py
@@ -0,0 +1,12 @@
+clothes = ["Jeans", "Shirt", "Jacket", "Shorts"]
+
+packing = clothes + ["Sock"]
+print(f"Total packing of items such as: {packing}")
+
+rest_clothes = clothes.pop()
+trip_days = 3
+
+total_rest_clothes = [rest_clothes] * trip_days
+print(f"Total rest clothes for the trip: {total_rest_clothes}")
+
+# Need more understanding
