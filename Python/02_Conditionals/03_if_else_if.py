ef4b0db7b972dfdf6f0c2ed933a2daa0f3801dce
kanchi29 <kanchisri29@gmail.com>
2026-08-10 22:07:43 +0530

    If-else if (elif) conditionals

---
+++ Python/02_Conditionals/03_if_else_if.py
@@ -0,0 +1,10 @@
+size = input("Enter the order size (Small/Medium/Large): ")
+
+if size == "small":
+    print("Cost for the coffee is Rs. 10")
+elif size == "medium":
+    print("Cost for the coffee is Rs. 15")
+elif size == "large":
+    print("Cost for the coffee is Rs. 20")
+else:
+    print("Unknown cup size")
