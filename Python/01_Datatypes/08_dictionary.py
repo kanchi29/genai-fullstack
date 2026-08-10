aba31b3f3b01b3cf0556462393679d6d578413cb
kanchi29 <kanchisri29@gmail.com>
2026-08-10 21:30:47 +0530

    Dictionaries basics

---
+++ Python/01_Datatypes/08_dictionary.py
@@ -0,0 +1,23 @@
+# Dictionary is a way of storing data and indexing in the form of strings instead of numbers
+menu = dict(Starter = 'Soup',
+            Main_Course = 'Butter Chicken',
+            Drink = 'Mojito',
+            Desert = 'Brownie')
+print(menu)
+
+# Another way of defining and then initialising:
+
+codes = {}
+codes["red"] = "Emergency"
+codes["yellow"] = "Attention"
+print(codes)
+
+del codes["red"] # deletion
+print(codes)
+
+# Many methods available for dictionaries as well
+
+
+#Some additional datatypes:
+# datetime, time, calender, timedelta(time duration)
+# collections
