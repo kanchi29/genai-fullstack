30ae0494e752468563a28426785794bff47bfe42
kanchi29 <kanchisri29@gmail.com>
2026-08-10 22:08:23 +0530

    else if - elif

--- Python/02_Conditionals/03_if_else_if.py
+++ Python/02_Conditionals/03_if_else_if.py
@@ -2,6 +2,7 @@ size = input("Enter the order size (Small/Medium/Large): ")
 
 if size == "small":
     print("Cost for the coffee is Rs. 10")
+    # else if is written as elif
 elif size == "medium":
     print("Cost for the coffee is Rs. 15")
 elif size == "large":
