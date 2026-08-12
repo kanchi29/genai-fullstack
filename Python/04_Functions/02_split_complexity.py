18d141695cc842d9e46814617069c80db9f6fbbb
kanchi29 <kanchisri29@gmail.com>
2026-08-12 15:36:47 +0530

    Features of Functions: Split the complex parts of a code into multiple sub-parts

---
+++ Python/04_Functions/02_split_complexity.py
@@ -0,0 +1,20 @@
+# Excercise: Preparing Sales Report
+
+def fetch_data ():
+    print("Sales data fetched")
+
+def validate_data():
+    print("Valid data extracted")
+
+def profit_loss():
+    print("Calculated the total profit and loss!")
+
+def generate_report():
+    fetch_data()
+    validate_data()
+    profit_loss()
+    print("Generating the monthly sales report...")
+
+generate_report()
+
+# Basically we split a bunch of complex tasks into individual functions
