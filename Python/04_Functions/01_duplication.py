36809b8c305e0eb9cbb7ec8ffb678c370bd55b81
kanchi29 <kanchisri29@gmail.com>
2026-08-12 15:36:20 +0530

    Features of Functions: Reduce duplication

---
+++ Python/04_Functions/01_duplication.py
@@ -0,0 +1,10 @@
+# every function starts with a keyword def followed by the function name and the parameter(s).
+
+# Syntax: def [function_name] ([parameters])
+
+def print_order(name, order):
+    print(f"{name}'s order is {order}!")
+
+print_order("Kanchi", "burger")
+print_order("Adarshita", "pizza")
+print_order("Abhishek", "noodles")
